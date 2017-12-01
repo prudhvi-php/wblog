3ef31228aaef045308e0aba8a4c160744e0e9e5a
prudhvi <prudhvi259@gmail.com>
2017-12-01 13:08:55 +0530

    new file added

--- my_recipes/app/Http/Controllers/HomeController.php
+++ my_recipes/app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
 
         $bpost->title = $request->title;
         $bpost->category = $request->category;
-        $bpost->description = $request->description;
+        $bpost->description = $request->description1;
         $bpost->tags = $request->tags;
 
         if($request->category == "Laravel"){
